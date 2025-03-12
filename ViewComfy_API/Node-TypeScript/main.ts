dc3de8a7735ae95079c23be368fc6af6c47b5287
GBieler <guillaume.bieler@gmail.com>
2025-03-12 11:20:33 +0000

    updated ts example

--- ViewComfy_API/Node-TypeScript/main.ts
+++ ViewComfy_API/Node-TypeScript/main.ts
@@ -3,37 +3,19 @@ import * as path from "path";
 import { infer, inferWithLogsStream } from "../../workflows/flux-consistent-characters/node-typescript/api";
 import { workflowApiParametersCreator } from "../../workflows/flux-consistent-characters/node-typescript/workflow_api_parameters_creator";
 
-const viewComfyUrl = "";
+const viewComfyUrl = "<ViewComfy api url>";
 
 // Move your main function logic into a route handler
 const generate = async () => {
     try {
         const params = {};
 
-        const poseSheet = await loadImageFile("pose_sheet.png");
-        params["625-inputs-image"] = poseSheet;
+        params["6-inputs-text"] = "A cat sorcerer"
 
-        const character = await loadImageFile("character.png");
-        params["626-inputs-image"] = character;
+        const inputImage = await loadImageFile("<path to image>");
+        params["52-inputs-image"] = inputImage;
 
-        const characterPrompt =
-            "a attractive woman, dark long hair, a women wearing a grey wool turtleneck sweater, brown eyes, jeans, brown boots, short medium long hair, chin long hair that looks like she just got up, She has a fair complexion, expressive brown eyes, Her makeup is natural, highlighting her soft features. she has slightly pink cheeks and a healthy skin tone";
-        params["594-inputs-string"] = characterPrompt;
-
-        params["82-inputs-upscale_by"] = 2;
-        params["426-inputs-string"] =
-            "a deep forest with oaks and pine trees ferns and bushes, national park, close up, overcast, close up, amateur photography, shot on iphone, candid photo";
-        params["436-inputs-string"] =
-            "a modern city during sunset, the sky is adorned by epic cloud formations, frontal close up, walking through the city, hard sunlight on face, Side lit, candid photography, dslr, evening, silhouette, moody, autumn, warm orange atmosphere, natural smile, amateur photography, shot on iphone, candid photo,  winking with one eye closed";
-        params["443-inputs-string"] =
-            "music video, color gel lighting, dark background, fog, colorful lighting, looking away from camera, stage lighting, concert stage, neon colors, silhouette, darkness, moody, amateur photography, shot on iphone, candid photo";
-        params["458-inputs-string"] =
-            "a vast desert landscape with distant mountains, the hard sunlight is illuminating the person from the side and casting shadows on to the white sand, blue sky, shadows, waving, close up, candid photography, shocked expression, side lit face, shocked expression with an open mouth, surprised face, amateur photography, shot on iphone, candid photo";
-        params["499-inputs-noise_seed"] = 384340151733840;
-        params["594-inputs-string"] =
-            "a attractive woman, dark long hair, a women wearing a grey wool turtleneck sweater, brown eyes, jeans, brown boots, short medium long hair, chin long hair that looks like she just got up, She has a fair complexion, expressive brown eyes, Her makeup is natural, highlighting her soft features. she has slightly pink cheeks and a healthy skin tone";
-        params["608-inputs-string"] =
-            "it is a masterpiece, amateur photography, shot on iphone";
+        params["3-inputs-steps"] = 1
 
         // Call the API and wait for the results
         // const result = await infer({
@@ -74,9 +56,8 @@ async function saveBlob(blob: Blob, filename: string): Promise<void> {
     await fs.writeFile(filename, buffer);
 }
 
-async function getWorkflowParametersForApi() {
+async function getWorkflowParametersForApi(workflowPath: string) {
     // Example usage with a workflow file
-    const workflowPath = "workflow_api.json";
     const workflowJson = JSON.parse(await fs.readFile(workflowPath, "utf-8"));
     const flattened = workflowApiParametersCreator(workflowJson);
 
@@ -86,5 +67,6 @@ async function getWorkflowParametersForApi() {
     );
 }
 
-// generate().catch(console.error);
-getWorkflowParametersForApi().catch(console.error);
+generate().catch(console.error);
+// const workflowPath = "/home/gbieler/GitHub/cloud-public/workflow_api.json";
+// getWorkflowParametersForApi(workflowPath).catch(console.error);
